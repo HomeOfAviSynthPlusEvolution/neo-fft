15067db00544a5ea29d5b57b631465b0042ccedd
Xinyue Lu <i@7086.in>
2026-09-21 01:43:30 -0700

    perf: implement 8-way AVX2 SIMD codelet for 16x16 2D FFT

---
+++ src/spectral/codelets/fft16x16.hpp
@@ -0,0 +1,410 @@
+#pragma once
+
+#include <complex>
+#include <cstddef>
+#include <cmath>
+
+#if defined(__AVX2__)
+#include <immintrin.h>
+#endif
+
+namespace neo_fft::codelet {
+
+using cfloat = std::complex<float>;
+
+namespace detail_16x16 {
+
+constexpr float PI = 3.14159265358979323846f;
+
+static const float W16_R[8] = {
+    std::cos(0.0f * PI / 8.0f), std::cos(1.0f * PI / 8.0f),
+    std::cos(2.0f * PI / 8.0f), std::cos(3.0f * PI / 8.0f),
+    std::cos(4.0f * PI / 8.0f), std::cos(5.0f * PI / 8.0f),
+    std::cos(6.0f * PI / 8.0f), std::cos(7.0f * PI / 8.0f),
+};
+
+static const float W16_I[8] = {
+    std::sin(0.0f * PI / 8.0f), std::sin(1.0f * PI / 8.0f),
+    std::sin(2.0f * PI / 8.0f), std::sin(3.0f * PI / 8.0f),
+    std::sin(4.0f * PI / 8.0f), std::sin(5.0f * PI / 8.0f),
+    std::sin(6.0f * PI / 8.0f), std::sin(7.0f * PI / 8.0f),
+};
+
+#if defined(__AVX2__)
+
+template <bool FORWARD>
+inline void fft16_8way_avx2(__m256* r, __m256* i) noexcept {
+  __m256 a0_r = r[0],  a0_i = i[0];
+  __m256 a1_r = r[8],  a1_i = i[8];
+  __m256 a2_r = r[4],  a2_i = i[4];
+  __m256 a3_r = r[12], a3_i = i[12];
+  __m256 a4_r = r[2],  a4_i = i[2];
+  __m256 a5_r = r[10], a5_i = i[10];
+  __m256 a6_r = r[6],  a6_i = i[6];
+  __m256 a7_r = r[14], a7_i = i[14];
+  __m256 a8_r = r[1],  a8_i = i[1];
+  __m256 a9_r = r[9],  a9_i = i[9];
+  __m256 a10_r = r[5], a10_i = i[5];
+  __m256 a11_r = r[13], a11_i = i[13];
+  __m256 a12_r = r[3], a12_i = i[3];
+  __m256 a13_r = r[11], a13_i = i[11];
+  __m256 a14_r = r[7], a14_i = i[7];
+  __m256 a15_r = r[15], a15_i = i[15];
+
+  #define BUTTERFLY2(x_r, x_i, y_r, y_i) { \
+    __m256 ur = x_r, ui = x_i; \
+    __m256 vr = y_r, vi = y_i; \
+    x_r = _mm256_add_ps(ur, vr); \
+    x_i = _mm256_add_ps(ui, vi); \
+    y_r = _mm256_sub_ps(ur, vr); \
+    y_i = _mm256_sub_ps(ui, vi); \
+  }
+
+  BUTTERFLY2(a0_r, a0_i, a1_r, a1_i);
+  BUTTERFLY2(a2_r, a2_i, a3_r, a3_i);
+  BUTTERFLY2(a4_r, a4_i, a5_r, a5_i);
+  BUTTERFLY2(a6_r, a6_i, a7_r, a7_i);
+  BUTTERFLY2(a8_r, a8_i, a9_r, a9_i);
+  BUTTERFLY2(a10_r, a10_i, a11_r, a11_i);
+  BUTTERFLY2(a12_r, a12_i, a13_r, a13_i);
+  BUTTERFLY2(a14_r, a14_i, a15_r, a15_i);
+
+  #define CMUL_SCALAR(out_r, out_i, in_r, in_i, wr_val, wi_val) { \
+    __m256 wr = _mm256_set1_ps(wr_val); \
+    __m256 wi = _mm256_set1_ps(FORWARD ? -(wi_val) : (wi_val)); \
+    out_r = _mm256_fmsub_ps(in_r, wr, _mm256_mul_ps(in_i, wi)); \
+    out_i = _mm256_fmadd_ps(in_r, wi, _mm256_mul_ps(in_i, wr)); \
+  }
+
+  #define MUL_J(out_r, out_i, in_r, in_i) { \
+    if constexpr (FORWARD) { \
+      out_r = in_i; \
+      out_i = _mm256_sub_ps(_mm256_setzero_ps(), in_r); \
+    } else { \
+      out_r = _mm256_sub_ps(_mm256_setzero_ps(), in_i); \
+      out_i = in_r; \
+    } \
+  }
+
+  #define BUTTERFLY4(x0_r, x0_i, x1_r, x1_i, x2_r, x2_i, x3_r, x3_i) { \
+    BUTTERFLY2(x0_r, x0_i, x2_r, x2_i); \
+    __m256 v1r, v1i; \
+    MUL_J(v1r, v1i, x3_r, x3_i); \
+    __m256 u1r = x1_r, u1i = x1_i; \
+    x1_r = _mm256_add_ps(u1r, v1r); \
+    x1_i = _mm256_add_ps(u1i, v1i); \
+    x3_r = _mm256_sub_ps(u1r, v1r); \
+    x3_i = _mm256_sub_ps(u1i, v1i); \
+  }
+
+  BUTTERFLY4(a0_r, a0_i, a1_r, a1_i, a2_r, a2_i, a3_r, a3_i);
+  BUTTERFLY4(a4_r, a4_i, a5_r, a5_i, a6_r, a6_i, a7_r, a7_i);
+  BUTTERFLY4(a8_r, a8_i, a9_r, a9_i, a10_r, a10_i, a11_r, a11_i);
+  BUTTERFLY4(a12_r, a12_i, a13_r, a13_i, a14_r, a14_i, a15_r, a15_i);
+
+  #define BUTTERFLY8(x0_r, x0_i, x1_r, x1_i, x2_r, x2_i, x3_r, x3_i, \
+                     x4_r, x4_i, x5_r, x5_i, x6_r, x6_i, x7_r, x7_i) { \
+    BUTTERFLY2(x0_r, x0_i, x4_r, x4_i); \
+    __m256 v1r, v1i; \
+    CMUL_SCALAR(v1r, v1i, x5_r, x5_i, W16_R[2], W16_I[2]); \
+    __m256 u1r = x1_r, u1i = x1_i; \
+    x1_r = _mm256_add_ps(u1r, v1r); x1_i = _mm256_add_ps(u1i, v1i); \
+    x5_r = _mm256_sub_ps(u1r, v1r); x5_i = _mm256_sub_ps(u1i, v1i); \
+    __m256 v2r, v2i; \
+    MUL_J(v2r, v2i, x6_r, x6_i); \
+    __m256 u2r = x2_r, u2i = x2_i; \
+    x2_r = _mm256_add_ps(u2r, v2r); x2_i = _mm256_add_ps(u2i, v2i); \
+    x6_r = _mm256_sub_ps(u2r, v2r); x6_i = _mm256_sub_ps(u2i, v2i); \
+    __m256 v3r, v3i; \
+    CMUL_SCALAR(v3r, v3i, x7_r, x7_i, W16_R[6], W16_I[6]); \
+    __m256 u3r = x3_r, u3i = x3_i; \
+    x3_r = _mm256_add_ps(u3r, v3r); x3_i = _mm256_add_ps(u3i, v3i); \
+    x7_r = _mm256_sub_ps(u3r, v3r); x7_i = _mm256_sub_ps(u3i, v3i); \
+  }
+
+  BUTTERFLY8(a0_r, a0_i, a1_r, a1_i, a2_r, a2_i, a3_r, a3_i, \
+             a4_r, a4_i, a5_r, a5_i, a6_r, a6_i, a7_r, a7_i);
+  BUTTERFLY8(a8_r, a8_i, a9_r, a9_i, a10_r, a10_i, a11_r, a11_i, \
+             a12_r, a12_i, a13_r, a13_i, a14_r, a14_i, a15_r, a15_i);
+
+  BUTTERFLY2(a0_r, a0_i, a8_r, a8_i);
+  r[0] = a0_r; i[0] = a0_i;
+  r[8] = a8_r; i[8] = a8_i;
+
+  for (int k = 1; k < 8; ++k) {
+    __m256 ak_r, ak_i, bk_r, bk_i;
+    if (k == 1) { ak_r = a1_r; ak_i = a1_i; bk_r = a9_r; bk_i = a9_i; }
+    else if (k == 2) { ak_r = a2_r; ak_i = a2_i; bk_r = a10_r; bk_i = a10_i; }
+    else if (k == 3) { ak_r = a3_r; ak_i = a3_i; bk_r = a11_r; bk_i = a11_i; }
+    else if (k == 4) { ak_r = a4_r; ak_i = a4_i; bk_r = a12_r; bk_i = a12_i; }
+    else if (k == 5) { ak_r = a5_r; ak_i = a5_i; bk_r = a13_r; bk_i = a13_i; }
+    else if (k == 6) { ak_r = a6_r; ak_i = a6_i; bk_r = a14_r; bk_i = a14_i; }
+    else { ak_r = a7_r; ak_i = a7_i; bk_r = a15_r; bk_i = a15_i; }
+
+    __m256 vr, vi;
+    if (k == 4) {
+      MUL_J(vr, vi, bk_r, bk_i);
+    } else {
+      CMUL_SCALAR(vr, vi, bk_r, bk_i, W16_R[k], W16_I[k]);
+    }
+    r[k]     = _mm256_add_ps(ak_r, vr);
+    i[k]     = _mm256_add_ps(ak_i, vi);
+    r[k + 8] = _mm256_sub_ps(ak_r, vr);
+    i[k + 8] = _mm256_sub_ps(ak_i, vi);
+  }
+
+  #undef BUTTERFLY2
+  #undef BUTTERFLY4
+  #undef BUTTERFLY8
+  #undef CMUL_SCALAR
+  #undef MUL_J
+}
+
+inline void transpose8x8_ps(__m256& r0, __m256& r1, __m256& r2, __m256& r3,
+                            __m256& r4, __m256& r5, __m256& r6, __m256& r7) noexcept {
+  __m256 t0 = _mm256_unpacklo_ps(r0, r1);
+  __m256 t1 = _mm256_unpackhi_ps(r0, r1);
+  __m256 t2 = _mm256_unpacklo_ps(r2, r3);
+  __m256 t3 = _mm256_unpackhi_ps(r2, r3);
+  __m256 t4 = _mm256_unpacklo_ps(r4, r5);
+  __m256 t5 = _mm256_unpackhi_ps(r4, r5);
+  __m256 t6 = _mm256_unpacklo_ps(r6, r7);
+  __m256 t7 = _mm256_unpackhi_ps(r6, r7);
+
+  __m256 s0 = _mm256_shuffle_ps(t0, t2, _MM_SHUFFLE(1, 0, 1, 0));
+  __m256 s1 = _mm256_shuffle_ps(t0, t2, _MM_SHUFFLE(3, 2, 3, 2));
+  __m256 s2 = _mm256_shuffle_ps(t1, t3, _MM_SHUFFLE(1, 0, 1, 0));
+  __m256 s3 = _mm256_shuffle_ps(t1, t3, _MM_SHUFFLE(3, 2, 3, 2));
+  __m256 s4 = _mm256_shuffle_ps(t4, t6, _MM_SHUFFLE(1, 0, 1, 0));
+  __m256 s5 = _mm256_shuffle_ps(t4, t6, _MM_SHUFFLE(3, 2, 3, 2));
+  __m256 s6 = _mm256_shuffle_ps(t5, t7, _MM_SHUFFLE(1, 0, 1, 0));
+  __m256 s7 = _mm256_shuffle_ps(t5, t7, _MM_SHUFFLE(3, 2, 3, 2));
+
+  r0 = _mm256_permute2f128_ps(s0, s4, 0x20);
+  r1 = _mm256_permute2f128_ps(s1, s5, 0x20);
+  r2 = _mm256_permute2f128_ps(s2, s6, 0x20);
+  r3 = _mm256_permute2f128_ps(s3, s7, 0x20);
+  r4 = _mm256_permute2f128_ps(s0, s4, 0x31);
+  r5 = _mm256_permute2f128_ps(s1, s5, 0x31);
+  r6 = _mm256_permute2f128_ps(s2, s6, 0x31);
+  r7 = _mm256_permute2f128_ps(s3, s7, 0x31);
+}
+
+inline void batch_r2c_avx2(std::size_t batch, const float* in, std::size_t in_dist,
+                           std::size_t in_stride, cfloat* out, std::size_t out_dist,
+                           std::size_t out_stride) noexcept {
+  for (std::size_t b = 0; b < batch; ++b) {
+    const float* blk_in = in + b * in_dist;
+    cfloat* blk_out = out + b * out_dist;
+
+    // 1. Horizontal pass directly to blk_out
+    for (int g = 0; g < 2; ++g) {
+      const int y_base = g * 8;
+      __m256 left[8], right[8];
+      for (int y = 0; y < 8; ++y) {
+        const float* src = blk_in + (y_base + y) * in_stride;
+        left[y]  = _mm256_loadu_ps(src);
+        right[y] = _mm256_loadu_ps(src + 8);
+      }
+      transpose8x8_ps(left[0], left[1], left[2], left[3], left[4], left[5], left[6], left[7]);
+      transpose8x8_ps(right[0], right[1], right[2], right[3], right[4], right[5], right[6], right[7]);
+
+      __m256 r[16], i[16];
+      for (int x = 0; x < 8; ++x) {
+        r[x]     = left[x];
+        r[x + 8] = right[x];
+        i[x]     = _mm256_setzero_ps();
+        i[x + 8] = _mm256_setzero_ps();
+      }
+
+      fft16_8way_avx2<true>(r, i);
+
+      for (int kx = 0; kx < 9; ++kx) {
+        alignas(32) float tr[8], ti[8];
+        _mm256_store_ps(tr, r[kx]);
+        _mm256_store_ps(ti, i[kx]);
+        for (int y = 0; y < 8; ++y) {
+          blk_out[(y_base + y) * out_stride + kx] = cfloat(tr[y], ti[y]);
+        }
+      }
+    }
+
+    // 2. Vertical pass in-place on blk_out
+    {
+      __m256 r[16], i[16];
+      for (int y = 0; y < 16; ++y) {
+        const cfloat* src = blk_out + y * out_stride;
+        alignas(32) float tr[8], ti[8];
+        for (int kx = 0; kx < 8; ++kx) {
+          tr[kx] = src[kx].real();
+          ti[kx] = src[kx].imag();
+        }
+        r[y] = _mm256_load_ps(tr);
+        i[y] = _mm256_load_ps(ti);
+      }
+      fft16_8way_avx2<true>(r, i);
+      for (int ky = 0; ky < 16; ++ky) {
+        alignas(32) float tr[8], ti[8];
+        _mm256_store_ps(tr, r[ky]);
+        _mm256_store_ps(ti, i[ky]);
+        cfloat* dst = blk_out + ky * out_stride;
+        for (int kx = 0; kx < 8; ++kx) {
+          dst[kx] = cfloat(tr[kx], ti[kx]);
+        }
+      }
+    }
+    {
+      __m256 r[16], i[16];
+      for (int y = 0; y < 16; ++y) {
+        const cfloat* src = blk_out + y * out_stride;
+        r[y] = _mm256_set1_ps(src[8].real());
+        i[y] = _mm256_set1_ps(src[8].imag());
+      }
+      fft16_8way_avx2<true>(r, i);
+      for (int ky = 0; ky < 16; ++ky) {
+        alignas(32) float tr[8], ti[8];
+        _mm256_store_ps(tr, r[ky]);
+        _mm256_store_ps(ti, i[ky]);
+        blk_out[ky * out_stride + 8] = cfloat(tr[0], ti[0]);
+      }
+    }
+  }
+}
+
+inline void batch_c2r_avx2(std::size_t batch, const cfloat* in, std::size_t in_dist,
+                           std::size_t in_stride, float* out, std::size_t out_dist,
+                           std::size_t out_stride, float fct) noexcept {
+  alignas(64) float inter_r[16][9];
+  alignas(64) float inter_i[16][9];
+  const __m256 vfct = _mm256_set1_ps(fct);
+
+  for (std::size_t b = 0; b < batch; ++b) {
+    const cfloat* blk_in = in + b * in_dist;
+    float* blk_out = out + b * out_dist;
+
+    // 1. Vertical 1D IFFT to inter buffer
+    {
+      __m256 r[16], i[16];
+      for (int y = 0; y < 16; ++y) {
+        const cfloat* src = blk_in + y * in_stride;
+        alignas(32) float tr[8], ti[8];
+        for (int kx = 0; kx < 8; ++kx) {
+          tr[kx] = src[kx].real();
+          ti[kx] = src[kx].imag();
+        }
+        r[y] = _mm256_load_ps(tr);
+        i[y] = _mm256_load_ps(ti);
+      }
+      fft16_8way_avx2<false>(r, i);
+      for (int ky = 0; ky < 16; ++ky) {
+        alignas(32) float tr[8], ti[8];
+        _mm256_store_ps(tr, r[ky]);
+        _mm256_store_ps(ti, i[ky]);
+        for (int kx = 0; kx < 8; ++kx) {
+          inter_r[ky][kx] = tr[kx];
+          inter_i[ky][kx] = ti[kx];
+        }
+      }
+    }
+    {
+      __m256 r[16], i[16];
+      for (int y = 0; y < 16; ++y) {
+        const cfloat* src = blk_in + y * in_stride;
+        r[y] = _mm256_set1_ps(src[8].real());
+        i[y] = _mm256_set1_ps(src[8].imag());
+      }
+      fft16_8way_avx2<false>(r, i);
+      for (int ky = 0; ky < 16; ++ky) {
+        alignas(32) float tr[8], ti[8];
+        _mm256_store_ps(tr, r[ky]);
+        _mm256_store_ps(ti, i[ky]);
+        inter_r[ky][8] = tr[0];
+        inter_i[ky][8] = ti[0];
+      }
+    }
+
+    // 2. Horizontal 1D IFFT directly to blk_out
+    for (int g = 0; g < 2; ++g) {
+      const int y_base = g * 8;
+      __m256 r[16], i[16];
+      for (int kx = 0; kx < 9; ++kx) {
+        alignas(32) float tr[8], ti[8];
+        for (int y = 0; y < 8; ++y) {
+          tr[y] = inter_r[y_base + y][kx];
+          ti[y] = inter_i[y_base + y][kx];
+        }
+        r[kx] = _mm256_load_ps(tr);
+        i[kx] = _mm256_load_ps(ti);
+      }
+      const __m256 vzero = _mm256_setzero_ps();
+      for (int kx = 1; kx < 8; ++kx) {
+        r[16 - kx] = r[kx];
+        i[16 - kx] = _mm256_sub_ps(vzero, i[kx]);
+      }
+
+      fft16_8way_avx2<false>(r, i);
+
+      for (int x = 0; x < 16; ++x) {
+        r[x] = _mm256_mul_ps(r[x], vfct);
+      }
+
+      transpose8x8_ps(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7]);
+      transpose8x8_ps(r[8], r[9], r[10], r[11], r[12], r[13], r[14], r[15]);
+
+      for (int y = 0; y < 8; ++y) {
+        float* dst = blk_out + (y_base + y) * out_stride;
+        _mm256_storeu_ps(dst, r[y]);
+        _mm256_storeu_ps(dst + 8, r[y + 8]);
+      }
+    }
+  }
+}
+
+#endif
+
+} // namespace detail_16x16
+
+inline void batch_fft16x16_r2c(std::size_t batch, const float* in, std::size_t in_dist,
+                               std::size_t in_stride, cfloat* out, std::size_t out_dist,
+                               std::size_t out_stride) noexcept {
+#if defined(__AVX2__)
+  detail_16x16::batch_r2c_avx2(batch, in, in_dist, in_stride, out, out_dist, out_stride);
+#else
+  (void)batch;
+  (void)in;
+  (void)in_dist;
+  (void)in_stride;
+  (void)out;
+  (void)out_dist;
+  (void)out_stride;
+#endif
+}
+
+inline void batch_fft16x16_c2r(std::size_t batch, const cfloat* in, std::size_t in_dist,
+                               std::size_t in_stride, float* out, std::size_t out_dist,
+                               std::size_t out_stride, float fct) noexcept {
+#if defined(__AVX2__)
+  detail_16x16::batch_c2r_avx2(batch, in, in_dist, in_stride, out, out_dist, out_stride, fct);
+#else
+  (void)batch;
+  (void)in;
+  (void)in_dist;
+  (void)in_stride;
+  (void)out;
+  (void)out_dist;
+  (void)out_stride;
+  (void)fct;
+#endif
+}
+
+inline void fft16x16_r2c(const float* in, std::size_t in_stride, cfloat* out,
+                         std::size_t out_stride) noexcept {
+  batch_fft16x16_r2c(1, in, 256, in_stride, out, 144, out_stride);
+}
+
+inline void fft16x16_c2r(const cfloat* in, std::size_t in_stride, float* out,
+                         std::size_t out_stride, float fct) noexcept {
+  batch_fft16x16_c2r(1, in, 144, in_stride, out, 256, out_stride, fct);
+}
+
+} // namespace neo_fft::codelet
--- src/spectral/fft_backend.cpp
+++ src/spectral/fft_backend.cpp
@@ -1,4 +1,5 @@
 #include "spectral/fft_backend.hpp"
+#include "spectral/codelets/fft16x16.hpp"
 
 #include <algorithm>
 #include <array>
@@ -184,6 +185,12 @@ inline void ensure_plans(int height, int width) {
 
 void r2c(int height, int width, const float* in, std::size_t in_row_stride, std::complex<float>* out,
          std::size_t out_row_stride) {
+#if defined(__AVX2__)
+  if (height == 16 && width == 16) {
+    codelet::fft16x16_r2c(in, in_row_stride, out, out_row_stride);
+    return;
+  }
+#endif
   ensure_plans(height, width);
   ScratchScope scope;
   const pf::shape_t shape{std::size_t(height), std::size_t(width)}, axes{0, 1};
@@ -194,6 +201,12 @@ void r2c(int height, int width, const float* in, std::size_t in_row_stride, std:
 
 void c2r(int height, int width, const std::complex<float>* in, std::size_t in_row_stride, float* out,
          std::size_t out_row_stride, float fct) {
+#if defined(__AVX2__)
+  if (height == 16 && width == 16) {
+    codelet::fft16x16_c2r(in, in_row_stride, out, out_row_stride, fct);
+    return;
+  }
+#endif
   ensure_plans(height, width);
   ScratchScope scope;
   const pf::shape_t shape{std::size_t(height), std::size_t(width)}, axes{0, 1};
@@ -207,6 +220,12 @@ void batch_r2c(std::size_t batch, int height, int width, const float* in, std::s
                std::size_t out_row_stride) {
   if (batch == 0)
     return;
+#if defined(__AVX2__)
+  if (height == 16 && width == 16) {
+    codelet::batch_fft16x16_r2c(batch, in, in_dist, in_row_stride, out, out_dist, out_row_stride);
+    return;
+  }
+#endif
   if (batch == 1) {
     r2c(height, width, in, in_row_stride, out, out_row_stride);
     return;
@@ -225,6 +244,12 @@ void batch_c2r(std::size_t batch, int height, int width, const std::complex<floa
                std::size_t in_row_stride, float* out, std::size_t out_dist, std::size_t out_row_stride, float fct) {
   if (batch == 0)
     return;
+#if defined(__AVX2__)
+  if (height == 16 && width == 16) {
+    codelet::batch_fft16x16_c2r(batch, in, in_dist, in_row_stride, out, out_dist, out_row_stride, fct);
+    return;
+  }
+#endif
   if (batch == 1) {
     c2r(height, width, in, in_row_stride, out, out_row_stride, fct);
     return;
